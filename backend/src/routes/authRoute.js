50e5cb24e665839dc73fa0be20c59109f89aee50
saubhagya02-bit <saubhagyaushani03@gmail.com>
2026-03-10 11:39:26 +0530

    Implement API rate limiting using Arcjet

--- backend/src/routes/authRoute.js
+++ backend/src/routes/authRoute.js
@@ -1,12 +1,15 @@
 import express from "express";
 import { signup, login, logout, updateProfile } from "../controllers/authController.js";
 import { protectRoute } from "../middleware/authMiddleware.js";
+import { arcjetProtection } from "../middleware/arcjetMiddleware.js";
 
 const router = express.Router();
 
+router.use(arcjetProtection);
+
 router.post("/signup", signup);
 
-router.post("/login", login);
+router.post("/login",  login);
 
 router.post("/logout", logout);
 
