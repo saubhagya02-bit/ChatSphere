15623b66d69317ecb2b51dacf899de62ff1e08f3
saubhagya02-bit <saubhagyaushani03@gmail.com>
2026-03-10 13:47:45 +0530

    Create endpoints for sending and retrieving messages

--- backend/src/routes/messageRoute.js
+++ backend/src/routes/messageRoute.js
@@ -1,9 +1,21 @@
 import express from "express";
+import { 
+  getAllContacts,
+  getMessagesByUserId,
+  sendMessage,
+  getChatPartners
+} from "../controllers/messageController.js";
+
+import { protectRoute } from "../middleware/authMiddleware.js";
+import { arcjetProtection } from "../middleware/arcjetMiddleware.js";
 
 const router = express.Router();
 
-router.get("/send", (req, res) => {
-    res.send("Send message endpoint");
-});
+router.use(arcjetProtection, protectRoute);
+
+router.get("/contacts", protectRoute, getAllContacts);
+router.get("/chats", protectRoute, getChatPartners);
+router.get("/:id",  protectRoute, getMessagesByUserId);
+router.post("/send/:id",  protectRoute, sendMessage);
 
 export default router;
