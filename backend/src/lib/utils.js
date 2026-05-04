adce4e6d132d84903320ed68265e89514164fa13
saubhagya02-bit <saubhagyaushani03@gmail.com>
2026-05-04 18:01:45 +0530

    improve environment handling for JWT secret and restructure utils file

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -2,19 +2,19 @@ import jwt from "jsonwebtoken";
 import { ENV } from "./env.js";
 
 export const generateToken = (userId, res) => {
-    const { JWT_SECRET } = process.env;
-    if (!JWT_SECRET) {
-        throw new Error("JWT_SECRET is not configured");
-    }
-    const token = jwt.sign({userId}, JWT_SECRET, {
-        expiresIn: "7d",
-    });
+  const { JWT_SECRET } = ENV;
+  if (!JWT_SECRET) {
+    throw new Error("JWT_SECRET is not configured");
+  }
+  const token = jwt.sign({ userId }, JWT_SECRET, {
+    expiresIn: "7d",
+  });
 
-    res.cookie("jwt", token, {
-        maxAge: 7 * 24 * 60 * 60 * 1000,
-        httpOnly: true,
-        sameSite: "strict",
-        secure: ENV.NODE_ENV === "development" ? false : true,
-    });
-    return token;
+  res.cookie("jwt", token, {
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    httpOnly: true,
+    sameSite: "strict",
+    secure: ENV.NODE_ENV === "development" ? false : true,
+  });
+  return token;
 };
