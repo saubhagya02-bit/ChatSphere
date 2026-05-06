ae0b95b341db998ca19fa395b3d521957fd7fe81
saubhagya02-bit <saubhagyaushani03@gmail.com>
2026-05-06 14:23:13 +0530

    add compound indexes to Message schema

--- backend/src/models/message.js
+++ backend/src/models/message.js
@@ -1,32 +1,26 @@
 import mongoose from "mongoose";
 
 const messageSchema = new mongoose.Schema(
-    {
-        senderId: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "User",
-            required: true,
-        },
-
-        receiverId: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "User",
-            required: true,
-        },
-
-        text: {
-            type: String,
-            trim: true,
-            maxlength: 200
-        },
-
-        image: {
-            type: String,
-        },
+  {
+    senderId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+      index: true,
     },
-    { timestamps: true }
+    receiverId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+      index: true,
+    },
+    text: { type: String, trim: true, maxlength: 200 },
+    image: { type: String },
+  },
+  { timestamps: true },
 );
 
-const Message = mongoose.model("Message", messageSchema);
+messageSchema.index({ senderId: 1, receiverId: 1 });
+messageSchema.index({ receiverId: 1, senderId: 1 });
 
-export default Message;
+export default mongoose.model("Message", messageSchema);
