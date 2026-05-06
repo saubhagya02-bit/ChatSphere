1739fd9a59eed47e8a92929f7cf5febf60bb69c0
saubhagya02-bit <saubhagyaushani03@gmail.com>
2026-05-06 14:24:30 +0530

    use environment variable for JWT_SECRET in utils

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -2,19 +2,16 @@ import jwt from "jsonwebtoken";
 import { ENV } from "./env.js";
 
 export const generateToken = (userId, res) => {
-  const { JWT_SECRET } = ENV;
-  if (!JWT_SECRET) {
-    throw new Error("JWT_SECRET is not configured");
-  }
-  const token = jwt.sign({ userId }, JWT_SECRET, {
-    expiresIn: "7d",
-  });
+  if (!JWT_SECRET) throw new Error("JWT_SECRET is not configured");
+
+  const token = jwt.sign({ userId }, JWT_SECRET, { expiresIn: "7d" });
 
   res.cookie("jwt", token, {
     maxAge: 7 * 24 * 60 * 60 * 1000,
     httpOnly: true,
     sameSite: "strict",
-    secure: ENV.NODE_ENV === "development" ? false : true,
+    secure: ENV.NODE_ENV !== "development",
   });
+
   return token;
 };
