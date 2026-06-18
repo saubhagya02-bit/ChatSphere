89d639c65271e3de9ac71fc4afee61705148e556
saubhagya02-bit <saubhagyaushani03@gmail.com>
2026-06-18 17:23:22 +0530

    feat(routes): add block, unblock and clear chat API endpoints

--- backend/src/routes/messageRoute.js
+++ backend/src/routes/messageRoute.js
@@ -4,6 +4,9 @@ import {
   getMessagesByUserId,
   sendMessage,
   getChatPartners,
+  clearMessages,
+  blockUser,
+  unblockUser,
 } from "../controllers/messageController.js";
 import { protectRoute } from "../middleware/authMiddleware.js";
 import { arcjetProtection } from "../middleware/arcjetMiddleware.js";
@@ -14,6 +17,9 @@ router.use(arcjetProtection, protectRoute);
 
 router.get("/contacts", getAllContacts);
 router.get("/chats", getChatPartners);
+router.put("/block/:id", blockUser);
+router.put("/unblock/:id", unblockUser);
+router.delete("/clear/:id", clearMessages);
 router.get("/:id", getMessagesByUserId);
 router.post("/send/:id", sendMessage);
 
